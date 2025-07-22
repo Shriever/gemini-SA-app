ff6e372f73dc8159eb79cc6df3da1146bea5b1a6
Levi Lieberman <shriever@users.noreply.github.com>
2025-07-22 17:39:54 -0400

    Received and configured earnings data from seeking alpha

--- src/api/seekingAlpha.ts
+++ src/api/seekingAlpha.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const rapidApiKey = process.env.RAPID_API_KEY;
 
+const rapidApiKey = process.env.RAPID_API_KEY;
 const rapidApiHost = 'seeking-alpha.p.rapidapi.com';
 
 if (!rapidApiKey) {
@@ -27,13 +27,16 @@ const options = {
 };
 
 export async function getSeekingAlphaData() {
-    const symbol = 'medp'; // TODO will eventually take the symbol from user
+  const symbol = 'medp'; // TODO will eventually take the symbol from user
+  const medpId = '568851';
 
-    const stockId = await getSymbolId(symbol);
+  // const stockId = await getSymbolId(symbol);
 
-    makeCallToSeekingAlpha(options);
+  const earningsData = await getEarnings(medpId);
 }
 
+// @Params: symbol -> stock symbol is used to get the internal company id
+// Returns: Internal stock id to use in other API calls
 async function getSymbolId(symbol: string) {
   const getMetaDataUrl =
     'https://seeking-alpha.p.rapidapi.com/symbols/get-meta-data';
@@ -53,18 +56,88 @@ async function getSymbolId(symbol: string) {
   const id: string | undefined = response.data.id;
 
   if (!id) {
-    throw new Error("Did not receive id from the Seeking Alpha API")
+    throw new Error('Did not receive id from the Seeking Alpha API');
   }
 
   return id;
 }
 
-async function getEarnings(id: string) {
-    /**
-     * Data Fields Required
-     * - Announcement date: 
-     * - 
-     */
+// @params: companyId -> Internal id used in place of stock symbol
+// Returns: Object with 5 points of earnings data
+async function getEarnings(companyId: string) {
+  /**
+   * Data Fields Required
+   * response.estimates["<companyId>"]
+   * - Announcement date: ["<some_id>"]["eps_normalized_actual"]["0"]["0"]["period"]["advancedate"]
+   * - EPS Normalized Actual:data["<some_id>"]["eps_normalized_actual"]["0"]["0"]["dataitemvalue"]
+   * - EPS GAAP Actual: data["<some_id>"]["eps_gaap_actual"]["0"]["0"]["dataitemvalue"]
+   * - Revenue Actual: data["<some_id>"]["revenue_actual"]["0"]["0"]["dataitemvalue"]
+   * - Revenue Surprise: revenue_actual["0"][0].dataitemvalue - revenue_consensus_mean["0"][0].dataitemvalue
+   */
+  const earningsUrl =
+    'https://seeking-alpha.p.rapidapi.com/symbols/get-earnings';
+  const earningsOptions = {
+    method: 'GET',
+    url: earningsUrl,
+    params: {
+      ticker_ids: companyId,
+      period_type: 'quarterly',
+      relative_periods: '-1,0',
+      estimates_data_items:
+        'eps_gaap_actual,eps_normalized_actual,revenue_actual,revenue_consensus_mean',
+      revisions_data_items:
+        'eps_normalized_actual,eps_normalized_consensus_mean',
+    },
+    headers,
+  };
+
+  const response = await makeCallToSeekingAlpha(earningsOptions);
+
+  if (!response) {
+    throw new Error('Did not receive earnings data from the Seeking Alpha API');
+  }
+
+  try {
+    const estimates = response.estimates[companyId];
+    const actuals = {
+      epsNormalized: parseFloat(
+        estimates.eps_normalized_actual?.['0']?.[0]?.dataitemvalue ?? 'NaN'
+      ),
+      epsGAAP: parseFloat(
+        estimates.eps_gaap_actual?.['0']?.[0]?.dataitemvalue ?? 'NaN'
+      ),
+      revenueActual: parseFloat(
+        estimates.revenue_actual?.['0']?.[0]?.dataitemvalue ?? 'NaN'
+      ),
+      revenueEstimate: parseFloat(
+        estimates.revenue_consensus_mean?.['0']?.[0]?.dataitemvalue ?? 'NaN'
+      ),
+      announcementDate:
+        estimates.eps_normalized_actual?.['0']?.[0]?.advancedate ??
+        estimates.eps_normalized_actual?.['0']?.[0]?.effectivedate ??
+        'Unknown',
+    };
+
+    const revenueSurprise = actuals.revenueActual - actuals.revenueEstimate;
+
+    console.log(`
+        Announcement Date: ${actuals.announcementDate}
+        EPS Normalized Actual: ${actuals.epsNormalized}
+        EPS GAAP Actual: ${actuals.epsGAAP}
+        Revenue Actual: ${actuals.revenueActual}
+        Revenue Surprise: ${revenueSurprise}
+        `);
+
+    return {
+      announcementDate: actuals.announcementDate,
+      epsNormalizedActual: actuals.epsNormalized,
+      epsGAAPActual: actuals.epsGAAP,
+      revenueActual: actuals.revenueActual,
+      revenueSurprise: isNaN(revenueSurprise) ? 'NA' : revenueSurprise,
+    };
+  } catch (error) {
+    throw new Error(error as string);
+  }
 }
 
 async function makeCallToSeekingAlpha(options: any) {
